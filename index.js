23396dd5fb88d244391101db003f41e4cb292e87
Chris Best <chris@kik.com>
2017-02-17 13:34:59 -0500

    some basics

--- index.js
+++ index.js
@@ -2,12 +2,9 @@
 
 let util = require('util');
 let http = require('http');
-let Bot  = require('@kikinteractive/kik');
-let Util = require ('./util.js')
+let Bot = require('@kikinteractive/kik');
+let Util = require('./util.js');
 var redis = require('redis').createClient(process.env.REDIS_URL | 'redis://localhost:6379/1');
-
-console.log("asdf");
-redis.get('hi', console.log);
 
 let questions = [
     "Did you take 10k steps today?",
@@ -17,32 +14,123 @@ let questions = [
     "Did you take at least one big, beefy poop?"
 ]
 
-let noAnswers = ["No, I suck"]
-let yesAnswers = [
+let keyboardNo = ["No", "Nah", "No, I suck", "Nope"]
+let keyboardYes = [
+    "Yes",
     "Yup",
+    "Uh huh",
     "Of course",
     "Yes, I rock",
     "You bet",
+    "Hellz to the yeah",
     "Oh you best believe it"];
 
-let yesResponse = [
+let yesResponses = [
     "Excellent",
     "Satisfactory.",
+    "That's great. Really great",
     "It's the least you could do",
     "Ok",
     "Good",
     "Alright"
 ]
 
-let noResponse = [
+let noResponses = [
     ":(",
     "That's too bad",
     "Sad.",
     "Seriously?",
     "Ugh.",
-    "You do suck",
+    "Boo",
     "Why do I even bother?",
 ]
+
+let confusedResponses = [
+    "What?",
+    "Huh?",
+    "You're not making sense."
+]
+
+let stateHandlers = {
+    'new': (message, state) => {
+        var reply = Bot.Message.text("Hello there. Are you tired of being unhappy, of feeling your life is wasted, or just never being sure if you're good enough?")
+        reply.addTextResponse("Yes 😥");
+
+        state['state'] = 'new2';
+        putState(state, (err) => {
+            message.reply(reply);
+        });
+    },
+    'new2': (message, state) => {
+        var reply = Bot.Message.text("Of course you are! I can help. Ill ask you " + questions.length
+            + " questions every day, and being able to answer yes to all of them "
+            + "will give your life purpose.")
+        reply.addTextResponse("Finally");
+        reply.addTextResponse("Sounds awful");
+        message.reply(reply);
+        state['state'] = 'new3';
+        putState(state);
+    },
+    'new3': (message, state) => {
+
+        message.reply(Bot.Message.text("Great, lets get started"));
+
+        message.reply(Bot.Message.text(questions[0])
+            .addTextResponse(keyboardYes.randomElement())
+            .addTextResponse(keyboardNo.randomElement()))
+
+        state['state'] = 'questions';
+        state['answered'] = 0;
+        state['yes'] = 0;
+        putState(state);
+    },
+    'questions': (message, state) => {
+
+        if (keyboardYes.contains(message.body)) {
+            message.reply(yesResponses.randomElement())
+            state['answered']++;
+            state['yes']++;
+        }
+        else if (keyboardNo.contains(message.body)) {
+            message.reply(noResponses.randomElement())
+            state['answered']++
+        }
+        else {
+            message.reply(
+                Bot.Message.text(confusedResponses.randomElement())
+            );
+        }
+
+        if (state.answered >= questions.length) {
+            message.reply("That's it for today.");
+            state['state'] = 'waiting';
+        } else {
+            message.reply(Bot.Message.text(questions[state['answered']])
+                .addTextResponse(keyboardYes.randomElement())
+                .addTextResponse(keyboardNo.randomElement()));
+        }
+
+        putState(state);
+    }
+}
+
+function getState(user, callback) {
+    var skey = user + ":state";
+    redis.get(skey, (err, stateJSON) => {
+        if (err) return callback(err);
+        var state = { state: 'new', user: user }
+        if (stateJSON) {
+            state = JSON.parse(stateJSON);
+        }
+        callback(null, state);
+    });
+}
+
+function putState(state, callback) {
+    var skey = state['user'] + ":state";
+    redis.set(skey, JSON.stringify(state), callback);
+}
+
 
 // Configure the bot API endpoint, details for your bot
 let bot = new Bot({
@@ -51,18 +139,18 @@ let bot = new Bot({
     baseUrl: 'https://7dca070f.ngrok.io'
 });
 
-console.log("updating config")
 bot.updateBotConfiguration();
-console.log("updated.")
 
 bot.onTextMessage((message) => {
-    console.log(message)
-    message.reply(message.body);
+    getState(message.from, (err, state) => {
+        if (err) return console.log(err);
+        console.log(state);
+        stateHandlers[state.state](message, state);
+    });
 });
 
-bot.send(Bot.Message.text('sup foo!'), 'cb');
 
-// Set up your server and start listening
+console.log("Starting to listen")
 let server = http
     .createServer(bot.incoming())
     .listen(process.env.PORT || 8080);
--- util.js
+++ util.js
@@ -2,3 +2,7 @@
 Array.prototype.randomElement = function () {
     return this[Math.floor(Math.random() * this.length)]
 }
+
+Array.prototype.contains = function(element){
+    return this.indexOf(element) > -1;
+};
